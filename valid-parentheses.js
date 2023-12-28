4f1e192124c955c8078fb4f13ee23c276ea9354a
Dominik Loncar <dominikloncar@outlook.com>
2023-12-28 04:30:58 +0100

    Coded solution for 'Valid parentheses'

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -3,5 +3,52 @@
 // ()[({})]]
 
 const isValid = function(s) {
+
+  let count1 = 0;
+  let count2 = 0;
+  let count3 = 0;
+
+  for (let i=0; i<s.length; i++) {
+    console.log(s[i])
+
+    if(s[i] === '(')
+    {
+        count1++;
+    }
+    else if(s[i] === ')')
+    {
+        count1--;
+    }
+
     
+    if(s[i] === '[')
+    {
+        count2++;
+    }
+    else if(s[i] === ']')
+    {
+        count2--;
+    }
+
+    
+
+    if(s[i] === '{')
+    {
+        count3++;
+    }
+    else if(s[i] === '}')
+    {
+        count3--;
+    }
+
+    if(count1 < 0 || count2 < 0 || count3 < 0)
+    {
+        return false;
+    } 
+  }  
+
+  return true;
+
 };
+
+console.log(isValid('()[({})]]'));
