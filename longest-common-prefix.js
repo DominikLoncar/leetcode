fbe6b5aa873cb4d8975ccf7f2e1327735b175c60
Dominik Loncar <dominikloncar@outlook.com>
2023-12-29 00:59:24 +0100

    Added solution for 'longest-common-prefix'.

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -1,3 +1,40 @@
 const longestCommonPrefix = function(strs) {
     
+
+
+    let maxLetters = -1;
+    let longestCommonString = '';
+
+    for(let i=0; i<strs.length; i++)
+    {
+        if(strs[i].length > maxLetters)
+        {
+            maxLetters = strs[i].length;
+        }
+    }
+
+
+    for(let i=0; i<maxLetters; i++)
+    {  
+        let shouldBreak = false;
+
+        for(let j=1; j<strs.length; j++)
+        {
+            if(strs[j][i] !== strs[j-1][i])
+            {
+                shouldBreak=true;
+                break;
+            }
+        }
+        
+        if(shouldBreak)
+        {
+            break;
+        }
+        longestCommonString+=strs[0][i];
+    }
+
+    return longestCommonString;
 };
+
+console.log(longestCommonPrefix(["flower","flow","flight"]));
