915da8c4e845e4c893c6d3fb36ec0fc11738465d
Dominik Loncar <dominikloncar@outlook.com>
2023-12-29 00:38:36 +0100

    Edited comment.

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -1,6 +1,6 @@
 // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
-// ()[({})]]
+// ()[({}())]]()(({{} {)[)]}())))
 
 const isValid = function(s) {
 
