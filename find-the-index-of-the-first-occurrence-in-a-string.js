8b13c6c8f6da55777a1f8961240206e6ee65ad73
Dominik Loncar <dominikloncar@outlook.com>
2023-12-29 13:02:13 +0100

    Coded solution for 'find the indef of first occurance'.

--- find-the-index-of-the-first-occurrence-in-a-string.js
+++ find-the-index-of-the-first-occurrence-in-a-string.js
@@ -1,3 +1,27 @@
 const strStr = function(haystack, needle) {
-    
+    let indexOfFirstStringOccurance = -1;
+
+    for(let i=0; i<haystack.length - needle.length + 1; i++)
+    {
+        let flag = true;
+
+        for(let j=0; j<needle.length; j++)
+        {
+            if(haystack[i+j] !== needle[j])
+            {
+                flag = false;
+                break;
+            }
+        }
+
+        if(flag)
+        {
+            indexOfFirstStringOccurance = i;
+            break;
+        }
+    }
+
+    return indexOfFirstStringOccurance;
 };
+
+console.log(strStr('blablablaab','laa'));
