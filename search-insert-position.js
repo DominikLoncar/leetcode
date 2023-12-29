2355fd7d1789904bd3ba6a163869198d493b2025
Dominik Loncar <dominikloncar@outlook.com>
2023-12-29 13:18:10 +0100

    Coded solution for 'search-index-position'.

--- search-insert-position.js
+++ search-insert-position.js
@@ -1,3 +1,24 @@
-var searchInsert = function(nums, target) {
+const searchInsert = function(nums, target) {
     
+    let targetPosition = nums.length;
+
+    for(let i=0; i<nums.length-1; i++)
+    {
+        if(nums[i] === target)
+        {
+            targetPosition = i+1;
+            break;
+        }
+
+        if(target >= nums[i] && target <= nums[i+1])
+        {
+           
+            targetPosition = i+1;
+            break;
+        }
+    }
+
+    return targetPosition;
 };
+
+console.log(searchInsert([1,3,5,6],5));
