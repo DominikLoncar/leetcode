ac6ea752ace083ecfbdcd04dedfc2802364dc0cc
Dominik Loncar <dominikloncar@outlook.com>
2023-12-29 02:50:46 +0100

    Updated 'remove-element' function to be more clear and simple, using 'filter'.

--- remove-element.js
+++ remove-element.js
@@ -1,14 +1,5 @@
 const removeElement = function(nums, val) {
-
-    nums.map((num, i) => {
-
-        if(num === val)
-        {
-            nums.splice(i,1);
-        }
-    })
-
-    return nums;
+    return nums.filter(num => num !== val);
 };
 
-console.log(removeElement([0,1,2,2,3,0,4,2],2));
+console.log(removeElement([3,2,2,3],2));
