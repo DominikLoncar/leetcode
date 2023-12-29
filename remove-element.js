b46171cf81a778944eab19825a3151f1e379e9bd
Dominik Loncar <dominikloncar@outlook.com>
2023-12-29 02:27:33 +0100

    Created solution for 'remove-element'.

--- remove-element.js
+++ remove-element.js
@@ -1,3 +1,14 @@
 const removeElement = function(nums, val) {
-    
+
+    nums.map((num, i) => {
+
+        if(num === val)
+        {
+            nums.splice(i,1);
+        }
+    })
+
+    return nums;
 };
+
+console.log(removeElement([0,1,2,2,3,0,4,2],2));
