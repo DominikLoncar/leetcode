b5104f873791f324ed03e3da398c0989f7c0cd17
Dominik Loncar <dominikloncar@outlook.com>
2023-12-30 22:47:54 +0100

    Coded the solution for 'length of last word.'

--- length-of-last-word.js
+++ length-of-last-word.js
@@ -1,3 +1,30 @@
 const lengthOfLastWord = function(s) {
     
+    let foundFirstLetterFromBack = false;
+    let lastWordLength = 0;
+
+    for(let i=s.length-1; i>=0; i--)
+    {
+        if(!foundFirstLetterFromBack && s[i] === ' ')
+        {
+            continue;
+        }
+
+        if(foundFirstLetterFromBack && s[i] === ' ')
+        {
+            break;
+        }
+
+        if(!foundFirstLetterFromBack && s[i] !== ' ')
+        {
+            foundFirstLetterFromBack = true;
+        }
+
+        console.log(s[i]);
+        lastWordLength++;
+    }
+
+    return lastWordLength;
 };
+
+console.log(lengthOfLastWord('   fly me   to   the moon  '));
